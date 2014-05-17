a15087ae30b2264faf8d49c3fb49c4c74e503015
tomaszb <tbacewicz@gmail.com>
2014-05-16 22:14:12 -0700

    added exception logging

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from findgoodmovies import *
 
 app = Flask(__name__)
 Bootstrap(app)
+app.config['PROPAGATE_EXCEPTIONS'] = True
 
 @app.route('/')
 def index():
@@ -16,12 +17,14 @@ def search():
 	imdb_value = request.args.get("imdb-value")
 	metacritic_value = request.args.get("metacritic-value")
 	zipcode_value = request.args.get("zipcode-input")
+
 	
 	if imdb_value != None and metacritic_value != None and zipcode_value != None:
 		imdb = float(imdb_value)/10.0
 		metacrit = float(metacritic_value)
 		zipcode = zipcode_value
 		movies_filt = Findmovies_main(zipcode, imdb, metacrit)
+		print "what about here"
 		for each in movies_filt:
 			each.createLink(zipcode)
 
@@ -29,6 +32,7 @@ def search():
 
 		if return_json != None and return_json == "true":
 			testing = dictifyMovies(movies_filt)
+			print "is it here"
 			print testing
 			return jsonify(dictifyMovies(movies_filt))
 		else:
